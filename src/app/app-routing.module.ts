3b1fbb9daf38da19672ed9320c90b595b1c1785b
camerbe <tristanvellozzi@gmail.com>
2025-02-03 12:31:40 +0100

    Demo01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  ,
+  {
+    path:'demo01',
+    component: Demo01Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
